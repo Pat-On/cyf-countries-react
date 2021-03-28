33e6d18f311f98490343c18d3335e28b5a9a632d
PatOn <mahomed2278@gmail.com>
2021-03-28 19:19:12 +0100

    lvl 2

--- src/App.js
+++ src/App.js
@@ -1,7 +1,9 @@
 import "./App.css";
 
 import React, { useEffect, useState } from "react";
+
 import CountryElement from "./countryElement";
+import SearchBar from "./SearchBar";
 
 function App() {
   const [allCountries, allCountriesHandler] = useState([]);
@@ -14,10 +16,37 @@ function App() {
         allCountriesHandler(data);
         searchResultsHandler(data);
       });
-  });
+  }, []);
+
+  const searchCountryFunction = (e) => {
+    e.preventDefault();
+    // let arr = [];
+    // switch (girlsBoysEverybody) {
+    //   case 1:
+    //     arr = jsonData.filter((item) => item.sex === "f");
+    //     break;
+    //   case 2:
+    //     arr = jsonData.filter((item) => item.sex === "m");
+    //     break;
+    //   case 3:
+    //     arr = [...jsonData];
+    //     break;
+    //   default:
+    //     break;
+    // }
+    const query = e.target.value;
+    const filteredCountries = allCountries.filter((item) => {
+      return (
+        item.name.toUpperCase().includes(query.toUpperCase()) ||
+        item.capital.toUpperCase().includes(query.toUpperCase())
+      );
+    });
+    searchResultsHandler(filteredCountries);
+  };
 
   return (
     <div className="App">
+      <SearchBar searchFunction={searchCountryFunction} />
       <div className="countryContainer">
         {searchResults.map((item, index) => (
           <CountryElement key={index} item={item} />
